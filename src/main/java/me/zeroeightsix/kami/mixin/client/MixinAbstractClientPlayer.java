e5b6ade042f67a786d1da863993ced11437bc5b2
Bella <bella.who.two@gmail.com>
2019-12-26 20:54:52 -0500

    add existing work

--- src/main/java/me/zeroeightsix/kami/mixin/client/MixinAbstractClientPlayer.java
+++ src/main/java/me/zeroeightsix/kami/mixin/client/MixinAbstractClientPlayer.java
@@ -1,17 +1,18 @@
 package me.zeroeightsix.kami.mixin.client;
 
 import com.mojang.authlib.GameProfile;
+import me.zeroeightsix.kami.module.modules.bewwawho.capes.Capes;
 import me.zeroeightsix.kami.module.modules.bewwawho.capes.LayerCape;
 import me.zeroeightsix.kami.util.zeroeightysix.Wrapper;
 import net.minecraft.client.entity.AbstractClientPlayer;
 import net.minecraft.client.renderer.entity.RenderPlayer;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-//i love crystallinqq <3
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(AbstractClientPlayer.class)
 public class MixinAbstractClientPlayer {
@@ -25,4 +26,13 @@ public class MixinAbstractClientPlayer {
             //}
         }
     }
+
+//    private Capes hook;
+//    @Inject(method = "getLocationCape", at = @At("HEAD"), cancellable = true)
+//    public void changeCapeTexture(CallbackInfoReturnable<ResourceLocation> cir) {
+//        hook = new Capes();
+//        cir.setReturnValue(hook.bindTexture(Capes()));
+//    }
+
+
 }
---
+++ src/main/java/me/zeroeightsix/kami/module/modules/bewwawho/experimental/DiscordWebTest.java
@@ -0,0 +1,16 @@
+package me.zeroeightsix.kami.module.modules.bewwawho.experimental;
+
+import me.zeroeightsix.kami.module.Module;
+import me.zeroeightsix.kami.module.modules.bewwawho.util.DiscordWebhook;
+
+/***
+ * @author S-B99
+ */
+@Module.Info(name = "WebTest", category = Module.Category.EXPERIMENTAL, description = "Discord Webhook test")
+public class DiscordWebTest extends Module {
+
+//    public void onEnable() {
+//        new DiscordWebhook("https://discordapp.com/api/webhooks/657650838260416524/7_SYiOke8NSjJ00-ZVRw5-wRc9XzVgpuMeyoOhVYnaznSaOeOc_lNZr8IjUU1bMpOZEt").setContent("asd");
+//        this.disable();
+//    }
+}
